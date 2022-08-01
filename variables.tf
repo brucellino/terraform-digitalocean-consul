236b12905a1bffa82d23f5e1d72eb03859f9bf49
Bruce Becker <brucellino@protonmail.com>
2022-08-01 10:39:10 +0200

    feat(variables): add variables
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- variables.tf
+++ variables.tf
@@ -1,8 +1,53 @@
-# variables.tf
-# Use this file to declare the variables that the module will use.
-
-# A dummy variable is provided to force a test validation
-variable "dummy" {
+# consul-digitalocean variables.tf
+variable "vpc" {
   type        = string
-  description = "dummy variable"
+  description = "VPC we are deploying into"
+}
+
+variable "servers" {
+  type        = number
+  default     = 3
+  description = "number of server instances"
+}
+
+variable "project_name" {
+  type        = string
+  default     = "hashi"
+  description = "Name of the project in digitalocean"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "hashi"
+  description = "Name of the VPC we are deploying into"
+}
+
+variable "ssh_inbound_source_cidrs" {
+  type        = list(any)
+  description = "List of CIDRs from which we will allow ssh connections on port 22"
+  default     = []
+}
+
+variable "ssh_public_key_url" {
+  type        = string
+  description = "URL of of the public ssh key to add to the droplet"
+  default     = "https://github.com/brucellino.keys"
+}
+
+variable "droplet_size" {
+  type        = string
+  description = "Size of the droplet for Vault instances"
+  default     = "s-1vcpu-1gb"
+}
+
+variable "username" {
+  type        = string
+  description = "Name of the non-root user to add"
+  default     = "hashiuser"
+}
+
+variable "datacenter" {
+  type        = string
+  description = "Name of the Consul datacenter"
+  default     = "HashiDO"
 }
