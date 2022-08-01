af71c3c108151409b4bcdd4e88dd8387c0e91e2c
Bruce Becker <brucellino@protonmail.com>
2022-08-01 10:44:34 +0200

    fix(terraform): add version constraints to providers
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- terraform.tf
+++ terraform.tf
@@ -5,5 +5,17 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "2.21.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.3.2"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "3.8.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "3.0.1"
+    }
   }
 }
