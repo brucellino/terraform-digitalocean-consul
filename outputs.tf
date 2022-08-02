00f250489c5a0385d2eab7dcae840f8c6c6d7287
Bruce Becker <brucellino@protonmail.com>
2022-08-02 10:01:06 +0200

    fix(output): add descriptions to the outputs
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "server_public_ips" {
-  value = digitalocean_droplet.server[*].ipv4_address
+  description = "List of public IPs for the Consul servers"
+  value       = digitalocean_droplet.server[*].ipv4_address
 }
 
 output "agent_public_ips" {
-  value = digitalocean_droplet.agent[*].ipv4_address
+  description = "List of public IPs for the Consul agents"
+  value       = digitalocean_droplet.agent[*].ipv4_address
 }
 
 output "load_balancer_ip" {
-  value = digitalocean_loadbalancer.external.ip
+  description = "Public IP of the load balancer fronting the servers"
+  value       = digitalocean_loadbalancer.external.ip
 }
